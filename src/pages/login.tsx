839219ddcd6e4e6b70dc13d6316368b036746b71
sholeh007 <sholeh09@outlook.com>
2022-02-24 20:10:58 +0900

    fix bug

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const Login: NextPage = ({ providers }: any) => {
   return (
     <div className="flex flex-col justify-center items-center bg-gray-900 w-full min-h-screen">
-      <div className="mb-4">
+      <div className="mb-4 w-32 h-32 md:w-44 md:h-44">
         <Image
           src={"/img/spotify.svg"}
           width={180}
@@ -19,7 +19,7 @@ const Login: NextPage = ({ providers }: any) => {
       {Object.values(providers).map((provider: any, i) => (
         <Fragment key={i}>
           <button
-            className="bg-green-500 inline-block p-5 rounded-full text-gray-800 font-semibold"
+            className="bg-green-500 inline-block p-5 rounded-full text-gray-800 font-semibold text-sm md:text-base"
             onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
             Login {provider.name}
